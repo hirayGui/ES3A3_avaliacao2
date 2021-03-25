0219e50469d848f6df16e2fd0e843d6a6f8b4014
Guilherme Brito <hiraygui@gmail.com>
2021-03-25 20:57:36 -0300

    implementando classe Participacao

--- src/avaliacao/participacao/Participacao.java
+++ src/avaliacao/participacao/Participacao.java
@@ -1,5 +1,33 @@
 package avaliacao.participacao;
 
+import java.time.LocalDate;
+
 public class Participacao {
 
+	private int id, idFuncionario;
+	private LocalDate dataInicio, dataTermino;
+	
+	public Participacao(int id, int idFuncionario, LocalDate dataInicio, LocalDate dataTermino) {
+		this.id = id;
+		this.idFuncionario = idFuncionario;
+		this.dataInicio = dataInicio;
+		this.dataTermino = dataTermino;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+	
+	public LocalDate getDataTermino() {
+		return dataTermino;
+	}
+	
 }
